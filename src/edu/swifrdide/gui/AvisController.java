51afb43d6640cfaf7e805f21142feb3efcadc731
samielmejri <sami.elmejri@esprit.tn>
2023-02-27 22:05:03 +0100

    Etoiles affiche

--- src/edu/swifrdide/gui/AvisController.java
+++ src/edu/swifrdide/gui/AvisController.java
@@ -15,6 +15,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,6 +28,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -50,9 +52,15 @@ public class AvisController implements Initializable {
 
     @FXML
     private Rating rating;
+    
+    @FXML
+    private Rating rating2;
 
     @FXML
     private Label msg;
+    
+    @FXML
+    private Label msg2;
 
     @FXML
     private TableView<Avis> table;
@@ -79,21 +87,19 @@ public class AvisController implements Initializable {
     private Button btnSubmit;
 
     AvisCRUD pcm=new AvisCRUD();
+    private int ratingValue = 0;
 
     @FXML
     void handleButtonAction(ActionEvent event) {
   if(event.getSource() == btnAjouter){
               ajouterAvis();
           }
-        /*  else if(event.getSource() == btnModifier){
+         else if(event.getSource() == btnModifier){
               modifierAvis();
-          }
+          }  
           else if(event.getSource() == btnSupprimer){
               supprimerAvis();
-          }
-           else if(event.getSource() == btnSubmit){
-              System.out.println("Rating Given by user:" +rating.getRating());
-          } */
+          } 
       }
    
     @Override
@@ -116,10 +122,31 @@ public class AvisController implements Initializable {
               rating.ratingProperty().addListener(new ChangeListener<Number>() {
                 @Override
                 public void changed(ObservableValue<? extends Number> ov, Number old, Number newT) {
-                     msg.setText("Avis : "+newT);
-                  }
-            
+                    ratingValue = newT.intValue(); 
+                    msg.setText("Votre avis : "+newT+" etoiles");
+                   
+                    // Appel de la méthode pour calculer la moyenne des notes
+                double moyenne = calculerMoyenne();
+  
+                msg2.setText("Moyenne d'avis , " +getAvis().size()+" utilisateurs"); 
+
+    
+                
+                 // Display the average rating in a non-editable rating control
+           
+                 // Mettre à jour le rating2 avec la valeur de la moyenne
+            rating2.setRating(moyenne);
+        }
+            public double calculerMoyenne() {
+    int sum = 0;
+    for (Avis avis : getAvis()) {
+        sum += avis.getEtoile();
+    }
+    double moyenne = (double) sum / getAvis().size();
+    return moyenne;
+}
             });
+              
      }
      
      public void showAvis(){
@@ -156,7 +183,8 @@ public class AvisController implements Initializable {
               }
            return listM;
         }
-    public void ajouterAvis() {
+    
+     public void ajouterAvis() {
         Avis m = new Avis();
         String commentaire = txtCommentiare.getText();
         int etoile = (int) rating.getRating();
@@ -172,7 +200,7 @@ public class AvisController implements Initializable {
         }
 
         // Check for forbidden words
-if (commentaire.toLowerCase().contains("stupid") || commentaire.toLowerCase().contains("crazy")) {
+if (commentaire.toLowerCase().contains("stupid") || commentaire.toLowerCase().contains("crazy") || commentaire.toLowerCase().contains("dumb")) {
     Alert alert = new Alert(Alert.AlertType.WARNING);
     alert.setTitle("GROS MOTS !!");
     alert.setHeaderText("Hey ! Pas de gros mots s'il vous plaît, il y a des enfants qui regardent.");
@@ -219,11 +247,118 @@ if (commentaire.toLowerCase().contains("stupid") || commentaire.toLowerCase().co
    
     }
        
+ private void modifierAvis() {
+        Avis m = new Avis();
+        Avis av = table.getSelectionModel().getSelectedItem();
+        
+        
+            if(av == null){
+    // Créer une alerte de type WARNING pour demander à l'utilisateur de choisir un avis à modifier
+    Alert warning = new Alert(Alert.AlertType.WARNING);
+    warning.setTitle("Attention");
+    warning.setHeaderText("Vous devez sélectionner un avis à modifier.");
+    warning.showAndWait();
+    return;
+}        
+         m.setId(table.getSelectionModel().getSelectedItem().getId());
+               
+        String commentaire = txtCommentiare.getText();
+        int etoile = (int) rating.getRating();
 
+        // Check that required fields are not empty
+        if (commentaire.isEmpty() ||  etoile <= 0) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Missing fields");
+            alert.setHeaderText("All fields must be filled");
+            alert.showAndWait();
+            return;
+        }
+
+        // Check for forbidden words
+if (commentaire.toLowerCase().contains("stupid") || commentaire.toLowerCase().contains("crazy")) {
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("GROS MOTS !!");
+    alert.setHeaderText("Hey ! Pas de gros mots s'il vous plaît");
+    alert.showAndWait();
+    return;
+}
+        
+        // Set values for the Avis object
+        m.setCommentaire(commentaire);
+        m.setEtoile(etoile);
+        
+      
+        // Effectuer la modification
+    pcm.modifierAvis(m);
+    table.setItems(getAvis());
+
+              
+    // Add comment based on the rating
+    String message;
+    switch (etoile) {
+        case 1:
+            message = "Avez-vous eu une mauvaise journée ?";
+            break;
+        case 2:
+            message = "Le plafond est à cinq, vous savez ?";
+            break;
+        case 3:
+            message = "Merci mais vous êtes sûr que c'est la bonne note ?";
+            break;
+        case 4:
+            message = "Excellent, vous êtes presque parfait !";
+            break;
+        case 5:
+            message = "wow 5 étoiles ! Vous êtes incroyable !";
+            break;
+        default:
+            message = "Merci pour votre commentaire !";
+            break;
+    }
+        // Afficher une alerte de confirmation si la modification a réussi
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setHeaderText(message);
+     alert.setContentText("Modification a été effectuée avec succès");
+    alert.showAndWait();   
+    }  
+
+private void supprimerAvis(){
+    Avis m = new Avis();
+        Avis av = table.getSelectionModel().getSelectedItem();
+        
+        
+            if(av == null){
+    // Créer une alerte de type WARNING pour demander à l'utilisateur de choisir un avis à modifier
+    Alert warning = new Alert(Alert.AlertType.WARNING);
+    warning.setTitle("Attention");
+    warning.setHeaderText("Vous devez sélectionner un avis à modifier.");
+    warning.showAndWait();
+    return;
+}    
+
+// Créer une alerte de type CONFIRMATION pour demander à l'utilisateur s'il veut vraiment supprimer l'entreprise
+Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+confirmation.setTitle("Confirmation de suppression");
+confirmation.setHeaderText("Êtes-vous sûr de vouloir supprimer l'avis " + av.getId() + " ?");
+Optional<ButtonType> result = confirmation.showAndWait();
+
+if(result.isPresent() && result.get() == ButtonType.OK){
+    pcm.supprimer(av);
+    table.getItems().removeAll(av);
+    
+    // Créer une alerte de type INFORMATION pour informer l'utilisateur que la suppression a réussi
+    Alert success = new Alert(Alert.AlertType.INFORMATION);
+    success.setTitle("Suppression réussie");
+    success.setHeaderText("L'entreprise a été supprimée avec succès.");
+    success.showAndWait();
+}
+    } 
+} 
+        
 
 
 
     
-}
+
 
    
